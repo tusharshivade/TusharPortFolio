d93c21cf9f2c3eeaaf8c7602ebc01ac891373790
Tushar shivade <tusharshivade122@gmail.com>
2026-03-09 10:38:28 +0530

    portfolio push

---
+++ script.js
@@ -0,0 +1,38 @@
+const text = [
+"DevOps Engineer",
+"Linux Administrator",
+"AWS Cloud Learner"
+]
+
+let count = 0
+let index = 0
+let currentText = ""
+let letter = ""
+
+function type(){
+
+if(count === text.length){
+count = 0
+}
+
+currentText = text[count]
+letter = currentText.slice(0, ++index)
+
+document.querySelector(".typing").textContent = letter
+
+if(letter.length === currentText.length){
+
+count++
+index = 0
+
+setTimeout(type,1000)
+
+}else{
+
+setTimeout(type,120)
+
+}
+
+}
+
+type()
